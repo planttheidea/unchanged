6db7c7a212d5554ac332a31d7022f21dca1e4d5a
Tony Quetano <tony.quetano@planttheidea.com>
2017-12-20 21:38:06 -0500

    Fix keywords and remove react plugin from dependencies

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,17 +1,11 @@
 'use strict';
 
-const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const defaultConfig = require('./webpack.config');
 
 const ROOT = path.resolve(__dirname, '..');
-
-const babelRcPath = path.resolve(ROOT, '.babelrc');
-
-const babelRc = fs.readFileSync(babelRcPath, {encoding: 'utf8'});
-const developmentBabelConfig = JSON.parse(babelRc).env.development;
 
 module.exports = Object.assign({}, defaultConfig, {
   devServer: {
@@ -40,15 +34,6 @@ module.exports = Object.assign({}, defaultConfig, {
           options: Object.assign({}, rule.options, {
             emitError: undefined,
             failOnWarning: false
-          })
-        });
-      }
-
-      if (rule.loader === 'babel-loader') {
-        return Object.assign({}, rule, {
-          options: Object.assign({}, rule.options, developmentBabelConfig, {
-            babelrc: false,
-            presets: [...developmentBabelConfig.presets, 'react']
           })
         });
       }
